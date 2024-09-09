4fa8ea81496dd215113143f600c76e50eefb4dc5
before-Sunrise <unclejyj@gmail.com>
2024-09-09 10:40:19 +0800

    fix fe test case and low dictionary rewrite problem
    Signed-off-by: before-Sunrise <unclejyj@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/operator/physical/PhysicalTopNOperator.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/operator/physical/PhysicalTopNOperator.java
@@ -158,6 +158,12 @@ public class PhysicalTopNOperator extends PhysicalOperator {
             }
         }
 
+        for (ColumnRefOperator column : partitionByColumns) {
+            if (column.getUsedColumns().isIntersect(dictSet)) {
+                return true;
+            }
+        }
+
         return false;
     }
 
