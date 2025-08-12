0d1f0d270d4c165803dc3fd510375c0a44ac731b
before-Sunrise <unclejyj@gmail.com>
2025-08-12 16:54:26 +0800

    optimize prepare
    Signed-off-by: before-Sunrise <unclejyj@gmail.com>

--- be/src/util/starrocks_metrics.h
+++ be/src/util/starrocks_metrics.h
@@ -369,6 +369,7 @@ public:
     METRICS_DEFINE_THREAD_POOL(compact_pool);
     METRICS_DEFINE_THREAD_POOL(pindex_load);
     METRICS_DEFINE_THREAD_POOL(put_aggregate_metadata);
+    METRICS_DEFINE_THREAD_POOL(pip_prepare);
 
     METRIC_DEFINE_UINT_GAUGE(load_rpc_threadpool_size, MetricUnit::NOUNIT);
 
