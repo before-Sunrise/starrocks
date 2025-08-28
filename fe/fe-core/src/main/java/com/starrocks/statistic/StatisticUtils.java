399478fa7e293995fdac5bb73f67859cd57e1a26
before-Sunrise <unclejyj@gmail.com>
2025-08-28 10:25:20 +0800

    ban predicate column late materialize for statistics collection
    Signed-off-by: before-Sunrise <unclejyj@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/statistic/StatisticUtils.java
+++ fe/fe-core/src/main/java/com/starrocks/statistic/StatisticUtils.java
@@ -119,6 +119,7 @@ public class StatisticUtils {
         context.getSessionVariable().setCboCteReuse(true);
         context.getSessionVariable().setCboCTERuseRatio(0);
         context.getSessionVariable().setEnablePlanSerializeConcurrently(false);
+        context.getSessionVariable().setEnablePredicateColLateMaterialize(false);
 
         WarehouseManager manager = GlobalStateMgr.getCurrentState().getWarehouseMgr();
         Warehouse warehouse = manager.getBackgroundWarehouse();
