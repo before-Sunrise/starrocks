067bc8f2472ddac427bc0ca759d565e8144efe32
before-Sunrise <unclejyj@gmail.com>
2025-09-08 16:56:23 +0800

    fix sv
    Signed-off-by: before-Sunrise <unclejyj@gmail.com>

--- be/src/storage/tablet_reader_params.h
+++ be/src/storage/tablet_reader_params.h
@@ -108,6 +108,7 @@ struct TabletReaderParams {
     TTableSampleOptions sample_options;
     bool enable_join_runtime_filter_pushdown = false;
     bool enable_predicate_col_late_materialize = false;
+    bool enable_predicate_order_sorting = false;
 
 public:
     std::string to_string() const;
