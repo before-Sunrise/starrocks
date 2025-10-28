eaedb792717024ee32b21868822bfe18f5497067
before-Sunrise <unclejyj@gmail.com>
2025-10-28 13:53:06 +0800

    turn on sv, fix clang-tidy
    Signed-off-by: before-Sunrise <unclejyj@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/qe/SessionVariable.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/SessionVariable.java
@@ -1980,7 +1980,7 @@ public class SessionVariable implements Serializable, Writable, Cloneable {
     private boolean enableInsertSelectExternalAutoRefresh = true;
     
     @VarAttr(name = ENABLE_PREDICATE_COL_LATE_MATERIALIZE)
-    private boolean enablePredicateColLateMaterialize = false;
+    private boolean enablePredicateColLateMaterialize = true;
 
     public int getCboPruneJsonSubfieldDepth() {
         return cboPruneJsonSubfieldDepth;
